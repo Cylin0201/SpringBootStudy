a751329823d9e8c969e37dc1f19367583d714c3a
Cylin01 <cylin0201@naver.com>
2025-09-01 13:10:45 +0900

    Second Commit

---
+++ src/main/java/com/backend/ureca/cylin0201/startspring/StartspringApplication.java
@@ -0,0 +1,13 @@
+package com.backend.ureca.cylin0201.startspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/ureca/cylin0201/startspring/StartspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ureca.cylin0201.startspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

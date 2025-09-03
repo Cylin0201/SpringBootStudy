a43f62316e2ad99146190100edb0c6dff6f3a9c5
Cylin01 <cylin0201@naver.com>
2025-09-03 15:36:12 +0900

    Initialize Project

--- src/main/java/com/backend/ureca/cylin0201/startspring/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.backend.ureca.cylin0201.startspring.dto;
-
-import lombok.Getter;
-
-@Getter
-public class LoginDto {
-    private String id;
-    private String password;
-}
--- src/main/java/com/backend/ureca/cylin0201/startspring/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.ureca.cylin0201.startspring.repository;
-
-import com.backend.ureca.cylin0201.startspring.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
